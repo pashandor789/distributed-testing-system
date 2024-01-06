e46767587a9568407492e85904de81b1576fe2fe
Pablo Ivanov <ooomegalool>
2024-01-06 14:30:03 +0300

    [Tabasco HTTP] Deduplicated handlers code.

--- task_batcher_storage_coordinator/http/server.cpp
+++ task_batcher_storage_coordinator/http/server.cpp
@@ -1,8 +1,29 @@
 #include "server.h"
 
-#include "handler_callbacks.h"
+#include "handlers/builds_handler.h"
+#include "handlers/execute_scripts_handler.h"
+#include "handlers/init_scripts_handler.h"
+#include "handlers/create_build_handler.h"
+#include "handlers/upload_execute_script_handler.h"
+#include "handlers/upload_init_script_handler.h"
+#include "handlers/upload_tests_handler.h"
 
 namespace NDTS::NTabasco {
+
+using THandlerCallback = std::function<crow::response(const crow::request&)>;
+
+template <typename THandler>
+THandlerCallback GetHandlerCallback(TTabascoHTTPServer* server) {
+    THandlerCallback callback =
+    [server](const crow::request& req) -> crow::response {
+        THandler handler;
+        crow::response resp;
+        handler.Handle(req, resp, TContext{.server = server});
+        return resp;
+    };
+
+    return callback;
+}
 
 TTabascoHTTPServer::TTabascoHTTPServer(const TTabascoServerConfig& config)
     : batchSize_(config.batch_size())
@@ -17,31 +38,31 @@ TTabascoHTTPServer::TTabascoHTTPServer(const TTabascoServerConfig& config)
 
 void TTabascoHTTPServer::InitHandlers() {
     CROW_ROUTE(app_, "/uploadTests").methods("POST"_method) (
-        GetLoadTestsCallback(this)
+        GetHandlerCallback<TUploadTestsHandler>(this)
     );
 
     CROW_ROUTE(app_, "/uploadInitScript").methods("POST"_method) (
-        GetUploadInitScriptCallback(this)
+        GetHandlerCallback<TUploadInitScriptHandler>(this)
     );
 
     CROW_ROUTE(app_, "/uploadExecuteScript").methods("POST"_method) (
-        GetUploadExecuteScriptCallback(this)
+        GetHandlerCallback<TUploadExecuteScriptHandler>(this)
     );
 
     CROW_ROUTE(app_, "/createBuild").methods("POST"_method) (
-        GetCreateBuildCallback(this)
+        GetHandlerCallback<TCreateBuildHandler>(this)
     );
 
     CROW_ROUTE(app_, "/initScripts").methods("GET"_method) (
-        GetInitScriptsCallback(this)
+        GetHandlerCallback<TInitScriptsHandler>(this)
     );
 
     CROW_ROUTE(app_, "/executeScripts").methods("GET"_method) (
-        GetExecuteScriptsCallback(this)
+        GetHandlerCallback<TExecuteScriptsHandler>(this)
     );
 
     CROW_ROUTE(app_, "/builds").methods("GET"_method) (
-        GetBuildsCallback(this)
+        GetHandlerCallback<TBuildsHandler>(this)
     );
 }
 
