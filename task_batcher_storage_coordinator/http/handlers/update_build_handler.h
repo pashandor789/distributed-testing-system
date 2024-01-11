ffd75554420975cd3736e8b4c3a2f1218c5f2d07
Pablo Ivanov <ooomegalool>
2024-01-11 11:09:36 +0300

    [Tabasco] Added updateBuild handler.

---
+++ task_batcher_storage_coordinator/http/handlers/update_build_handler.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <crow.h>
+
+#include "../server.h"
+
+namespace NDTS::NTabasco {
+
+class TUpdateBuildHandler {
+public:
+    void Handle(const crow::request& req, crow::response& res, const TContext& ctx);
+
+    bool Parse(const crow::request& req, crow::response& res);
+
+private:
+    std::string buildName_{};
+    std::string executeScript_{};
+    std::string initScript_{};
+    std::string description_{};
+};
+
+} // end of NDTS::TTabasco namespace
