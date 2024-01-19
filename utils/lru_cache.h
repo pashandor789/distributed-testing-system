5301cf1189202dc760858119de43d0ef36729d6e
Pavel <sosomaso90@gmail.com>
2024-01-19 21:58:11 +0300

    [Utils] LRUCache added.

---
+++ utils/lru_cache.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <mutex>
+#include <optional>
+#include <unordered_map>
+#include <list>
+
+template <typename TKey, typename TValue, size_t capacity>
+class TLRUCache {
+public:
+    void Insert(const TKey& key, const TValue& value) {
+        std::lock_guard lock{mtx_};
+
+        if (values_.contains(key)) {
+            return;
+        }
+
+        order_.push_front({key, value});
+
+        if (values_.size() > capacity) {
+            auto& [k, v] = order_.back();
+            values_.erase(k);
+        }
+    }
+
+    std::optional<TValue> Get(const TKey& key) {
+        std::lock_guard lock{mtx_};
+
+        auto it = values_.find(key);
+
+        if (it == values_.end()) {
+            std::cout << "Cachemiss!" << std::endl;
+            return std::nullopt;
+        }
+
+        TValue value = std::move(it->second->second);
+        order_.erase(it->second);
+        order_.push_front({key, value});
+        values_[key] = order_.begin();
+
+        std::cout << "Cache works!" << std::endl;
+
+        return value;
+    }
+
+    void Erase(const TKey& key) {
+        std::lock_guard lock{mtx_};
+
+        if (values_.contains(key)) {
+            order_.erase(values_[key]);
+            values_.erase(key);
+        }
+    }
+
+private:
+    std::mutex mtx_;
+
+    using TPair = std::pair<TKey, TValue>;
+    std::list<TPair> order_{};
+    std::unordered_map<TKey, typename std::list<TPair>::iterator> values_{};
+};
