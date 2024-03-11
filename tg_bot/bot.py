da2b2ad9ab0bbb89ba350ccb6390a76c3d218c28
maloriki <gudini848@gmail.com>
2024-03-11 14:51:43 +0300

    prikol

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -2,43 +2,76 @@ import telebot
 from telebot import types
 import pika
 import yaml
+import json
 
 with open("config.yaml", "r") as file:
     data = yaml.safe_load(file)
 token = data['token']
 bot = telebot.TeleBot(token)
 help_text = "Это бот для сдачи решений по программированию в контест систему (ДТС)"
+user_states = {}
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет это бот для проекта по питону")
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    button1 = types.InlineKeyboardButton("Сдать решение", callback_data='btn1')
-    button2 = types.InlineKeyboardButton("help", callback_data='btn2')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Сдать решение")
+    button2 = types.KeyboardButton("help")
     keyboard.add(button1, button2)
+    bot.send_message(message.chat.id, "Привет это бот для проекта по питону", reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_handler(call):
-    if call.data == 'btn1':
-        bot.send_message(call.message.chat.id, "Загрузите своей решение файлом, либо текстом.")
-    elif call.data == 'btn2':
-        bot.send_message(call.message.chat.id, help_text)
+@bot.message_handler(func=lambda message: message.text == 'Сдать решение')
+def handle_button1(message):
+    user_states[message.chat.id] = 'Сдать решение'
+    bot.register_next_step_handler(message, compilation_choice)
 
-    @bot.message_handler(content_types=['text'])
-    def handle_text(message):
-        text_input = message.text
-        bot.send_message(message.chat.id, f"You entered text: {text_input}")
 
-    @bot.message_handler(content_types=['document'])
-    def handle_file(message):
-        file = bot.get_file(message.document.file_id)
-        downloaded_file = bot.download_file(file.file_path)
-        with open("file_received.txt", 'wb') as file:
-            file.write(downloaded_file)
+def compilation_choice(message):
+    solution_text = message.text
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("gcc")
+    button2 = types.KeyboardButton("python")
+    keyboard.add(button1, button2)
+    bot.send_message(message.chat.id, "Выберите компилятор для решения.", reply_markup=keyboard)
+    bot.register_next_step_handler(message, lambda message: send_solution(message, solution_text))
 
-        bot.send_message(message.chat.id, "File successfully saved.")
+
+def send_solution(message, solution_text):
+    if message.text == 'gcc':
+        c_plus_plus_solution(message, solution_text)
+    elif message.text == 'python':
+        pass
+
+
+def c_plus_plus_solution(message, solution_text):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='test')
+    json_message = {
+        "submissionId": 0,
+        "buildId": 0,
+        "userData": solution_text,
+        "taskId": 0,
+        "memoryLimit": 10241024,
+        "cpuTimeLimitMilliSeconds": 2000
+    }
+
+    print(json.dumps(json_message))
+
+    channel.basic_publish(
+        exchange='',
+        routing_key='test',
+        body=json.dumps(json_message)
+    )
+    bot.send_message(message.chat.id, 'Ваше решение отправлено на сервер!')
+
+
+@bot.message_handler(func=lambda message: message.text == 'help')
+def handle_button2(message):
+    user_states[message.chat.id] = 'help'
+    bot.send_message(message.chat.id, help_text)
 
 
 bot.infinity_polling()
